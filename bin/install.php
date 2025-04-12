8f1f6ccbb7a6b08ec1a023e5d52ba93747897211
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2025-04-12 16:55:05 -0500

    Define `post-package-install` and `pre-package-uninstall` scripts.

---
+++ bin/install.php
@@ -0,0 +1,5 @@
+<?php
+
+declare(strict_types=1);
+
+echo 'Installing from ' . __DIR__ . PHP_EOL;
---
+++ bin/uninstall.php
@@ -0,0 +1,5 @@
+<?php
+
+declare(strict_types=1);
+
+echo 'Uninstalling from ' . __DIR__ . PHP_EOL;
