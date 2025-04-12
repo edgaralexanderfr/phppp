84d83443db5608d259442dea98c40a1c6ff2f26c
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2025-04-12 17:35:08 -0500

    v1.0.1

--- bin/install.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-echo 'Installing from ' . __DIR__ . PHP_EOL;
--- bin/uninstall.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-echo 'Uninstalling from ' . __DIR__ . PHP_EOL;
